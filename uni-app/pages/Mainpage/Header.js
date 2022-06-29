754a0b74839c48f9f325ede5da8125e0702b06cb
Gerald Freislich <freislichg@gmail.com>
2022-06-29 20:50:07 +0930

    safe

---
+++ uni-app/pages/Mainpage/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div className="Title" style={{
+            display: 'flex', justifyContent: 'center',
+        }}>
+            <h1>
+                University of Adelaide Interactive Map
+            </h1>
+        </div>
+    )
+}
---
+++ uni-app/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from './Mainpage/Header'
+
+export default function Home() {
+  return (
+    <div className='Mainpage'>
+      <Header />
+    </div>
+  )
+}
