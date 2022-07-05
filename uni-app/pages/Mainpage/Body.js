a03ba434efb9e7b00a704d0f73fe5091d906a912
Gerald Freislich <freislichg@gmail.com>
2022-07-05 11:30:29 +0930

    safe

---
+++ uni-app/pages/Mainpage/Body.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Image from "next/image"
+
+const Container = React.forwardRef((props, ref) => {
+    return (
+        <div ref={ref} style={{ overflowX: 'scroll', width: '500px' }} >
+            {props.children}
+        </div>
+    );
+} )
+
+export default function Body () {
+    const lazyRoot = React.useRef(null)
+
+    return (
+        <div className="Body">
+            <div className="Image" style={{display: 'Grid'}}>
+               {/* <Container ref={lazyRoot}>
+                    <Image lazyRoot={lazyRoot} src="/North_campus_map.png" width="500" height="500" /> 
+               </Container> */}
+               <img src="/North_campus_map.png"></img>
+            </div>
+        </div>
+    );
+}
