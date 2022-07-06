38bb9291e414c073ece3cb473407dc335f51a438
Gerald Freislich <freislichg@gmail.com>
2022-07-06 16:10:43 +0930

    safe commits

--- uni-app/pages/Mainpage/image_loader.js
+++ uni-app/pages/Mainpage/image_loader.js
@@ -5,11 +5,13 @@ export default function  ImageLoader () {
     return(
         <div className = 'image' style ={{display:'flex', justifyContent:'center',
                                             maxWidth: '100', width: 'auto', height: 'auto'}}>
-            <Image
-            src = '/North_Campus_Map.png'
-            width = '1500px'
-            height = '1500px'
-            />
+            <button onClick={get('./Node/a.out')} >
+                <Image
+                src = '/North_Campus_Map.png'
+                width = '1500px'
+                height = '1500px'
+                />
+            </button>
         </div>
     )
 
