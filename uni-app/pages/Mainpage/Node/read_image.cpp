34144cdab678cf81d345ee40eeea58fd6ecb978f
Gerald Freislich <freislichg@gmail.com>
2022-07-07 13:54:26 +0930

    safe

--- uni-app/pages/Mainpage/Node/read_image.cpp
+++ uni-app/pages/Mainpage/Node/read_image.cpp
@@ -7,19 +7,21 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../../../node_modules/stb/stb_image_write.h"
 
+// retrieving the image size (returns two element array containing width and height)
 int* rgb_image_size(){
     int width, height, bpp;
 
     uint8_t* rgb_image = stbi_load("map.png", &width, &height, &bpp, 1); // loading the image as uint8 type
     stbi_image_free(rgb_image);
 
-    int *arr = new int[2];
+    int *arr = new int[2]; 
     arr[0] = width;
     arr[1] = height;
 
     return arr;
 }
 
+// getting the pixel data from the map image and transferring that into a 2D array
 uint8_t **opening_image(uint8_t** image_matrix){
     int width, height, bpp, row = 1, column = 0;
 
@@ -37,14 +39,14 @@ uint8_t **opening_image(uint8_t** image_matrix){
         }
     }   
 
-    int counter = 0;
-    for(int r = 0; r < width; r++){
-        for(int c = 0; c < width; c++){
-            if(image_matrix[r][c] == 0){
-                std::cout << r << " " << c << "\n";
-            }
-        }
-    }
+    // int counter = 0;
+    // for(int r = 0; r < width; r++){
+    //     for(int c = 0; c < width; c++){
+    //         if(image_matrix[r][c] == 0){
+    //             std::cout << r << " " << c << "\n";
+    //         }
+    //     }
+    // }
     stbi_image_free(rgb_image);
     return image_matrix;
 }
@@ -70,10 +72,9 @@ void writingtodoc(uint8_t** image_matrix, int width, int height){
 void get_image(int width, int height, uint8_t** image_matrix){
     for (int r = 0; r < width; r++){
         for (int c = 0; c<height; c++){
-            if (image_matrix[r][c] <=10){
+            if (image_matrix[r][c] <= 10){
                 image_matrix[r][c] = 128;
             }
-
         }
     }
 
