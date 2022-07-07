ff5c00b9f03fac93acb9ef23ccf12cdf1c1e5e24
Gerald Freislich <freislichg@gmail.com>
2022-07-07 10:21:20 +0930

    Safe

--- uni-app/pages/Mainpage/Node/read_image.cpp
+++ uni-app/pages/Mainpage/Node/read_image.cpp
@@ -4,18 +4,14 @@
 #include "../../../node_modules/stb/stb_image.h"
 
 int main() {
-    int width, height, bpp;
+    int width, height, bpp, counter=0;
 
-    uint8_t* rgb_image = stbi_load("Pathway.png", &width, &height, &bpp, 3);
-    std::cout << width << "\n" << height << '\n';
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < height; j++){
-            if(rgb_image[i] == 0){
-                std::cout << i << " ";
-            }
+    uint8_t* rgb_image = stbi_load("Pathway.png", &width, &height, &bpp, 3); // loading the image as uint8 type
+    for(int i = 0; i < width*height; i++){
+        if(rgb_image[i] == 0){
+            std::cout <<  "Worked" << i << "\n";
         }
     }
     stbi_image_free(rgb_image);
-
     return 0;
 }
