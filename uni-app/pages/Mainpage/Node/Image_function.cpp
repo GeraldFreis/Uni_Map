c56385b86a88fef719084cfe5f328322457c6af8
Gerald Freislich <freislichg@gmail.com>
2022-07-07 12:00:08 +0930

    safe

--- uni-app/pages/Mainpage/Node/Image_function.cpp
+++ uni-app/pages/Mainpage/Node/Image_function.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+extern uint8_t **opening_image(uint8_t**);
+extern int* rgb_image_size();
+extern void writingtodoc(uint8_t**, int, int);
 
 int main() {
-    std::cout << "here" << "\n";
+    int *arr = rgb_image_size(); int width = arr[0]; int height = arr[1];
+
+    uint8_t **image_matrix =  new uint8_t*[height];
+    for(int i = 0; i < width; i++){image_matrix[i] = new uint8_t[width];}
+
+    image_matrix = opening_image(image_matrix);
+
+    writingtodoc(image_matrix, width, height);
+
+    for(int i = 0; i < width; i++){delete [] image_matrix[i];}
+    delete [] image_matrix;
+
 }
--- uni-app/pages/Mainpage/Node/read_image.cpp
+++ uni-app/pages/Mainpage/Node/read_image.cpp
@@ -2,13 +2,26 @@
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../../node_modules/stb/stb_image.h"
+#include <fstream>
+#include <istream>
 
-int main() {
+int* rgb_image_size(){
+    int width, height, bpp;
+
+    uint8_t* rgb_image = stbi_load("Pathway.png", &width, &height, &bpp, 3); // loading the image as uint8 type
+    stbi_image_free(rgb_image);
+
+    int *arr = new int[2];
+    arr[0] = width;
+    arr[1] = height;
+
+    return arr;
+}
+
+uint8_t **opening_image(uint8_t** image_matrix){
     int width, height, bpp, row = 1, column = 0;
 
     uint8_t* rgb_image = stbi_load("Pathway.png", &width, &height, &bpp, 3); // loading the image as uint8 type
-    uint8_t **image_matrix =  new uint8_t*[height];
-    for(int i = 0; i < width; i++){image_matrix[i] = new uint8_t[width];}
 
     for(int i = 0; i < width*height; i++){
         if(i == (row * width) - 1){
@@ -26,16 +39,29 @@ int main() {
     for(int r = 0; r < width; r++){
         for(int c = 0; c < width; c++){
             if(image_matrix[r][c] == 0){
-                std::cout << image_matrix[r][c] << "\n";
+                std::cout << r << " " << c << "\n";
             }
         }
     }
-    // std::cout << counter;
-
     stbi_image_free(rgb_image);
-
-    for(int i = 0; i < width; i++){delete [] image_matrix[i];}
-    delete [] image_matrix;
-    
-    return 0;
+    return image_matrix;
 }
+
+void writingtodoc(uint8_t** image_matrix, int width, int height){
+
+    std::ofstream MyFile("co_ordinates.txt");
+
+  // Write to the file
+  for(int i = 0; i < width; i++){
+    for(int j  = 0; j < height; j++){
+
+        if(image_matrix[i][j] == 255){
+            MyFile << i << ' ' << j << '\n';
+        }
+    }
+  }
+
+  // Close the file
+  MyFile.close();
+}
+
